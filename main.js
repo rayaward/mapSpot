180e4058abd3a44124333b63502d5c41c03865f3
Raya Ward <50107655+rayaward@users.noreply.github.com>
2019-07-18 15:13:11 -0700

    Initial file upload - map loads center on NOLA

---
+++ main.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXRsbWFwcm9vbSIsImEiOiJjamtiZzJ6dGIybTBkM3dwYXQ2c3lrMWs3In0.tJzsvakNHTk7G4iu73aP7g';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-90.071533, 29.951065], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
